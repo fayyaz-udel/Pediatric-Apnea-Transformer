c76c30096dcc20bf47d97029b3aed4113cfc1dc7
hamed <fayyaz@udel.edu>
2023-06-22 00:11:44 -0400

    missing

---
+++ missing_modality/Patches.py
@@ -0,0 +1,86 @@
+from keras import layers
+from config import *
+import tensorflow as tf
+import keras
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+class Patches(layers.Layer):
+    def __init__(self, patch_size=PATCH_SIZE, **kwargs):
+        super().__init__(**kwargs)
+        self.patch_size = patch_size
+
+        # Assuming the image has three channels each patch would be
+        # of size (patch_size, patch_size, 3).
+        self.resize = layers.Reshape((-1, patch_size * patch_size * 3))
+
+    def call(self, images):
+        # Create patches from the input images
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+
+        # Reshape the patches to (batch, num_patches, patch_area) and return it.
+        patches = self.resize(patches)
+        return patches
+
+    def show_patched_image(self, images, patches):
+        # This is a utility function which accepts a batch of images and its
+        # corresponding patches and help visualize one image and its patches
+        # side by side.
+        idx = np.random.choice(patches.shape[0])
+        print(f"Index selected: {idx}.")
+
+        plt.figure(figsize=(4, 4))
+        plt.imshow(keras.utils.array_to_img(images[idx]))
+        plt.axis("off")
+        plt.show()
+
+        n = int(np.sqrt(patches.shape[1]))
+        plt.figure(figsize=(4, 4))
+        for i, patch in enumerate(patches[idx]):
+            ax = plt.subplot(n, n, i + 1)
+            patch_img = tf.reshape(patch, (self.patch_size, self.patch_size, 3))
+            plt.imshow(keras.utils.img_to_array(patch_img))
+            plt.axis("off")
+        plt.show()
+
+        # Return the index chosen to validate it outside the method.
+        return idx
+
+    # taken from https://stackoverflow.com/a/58082878/10319735
+    def reconstruct_from_patch(self, patch):
+        # This utility function takes patches from a *single* image and
+        # reconstructs it back into the image. This is useful for the train
+        # monitor callback.
+        num_patches = patch.shape[0]
+        n = int(np.sqrt(num_patches))
+        patch = tf.reshape(patch, (num_patches, self.patch_size, self.patch_size, 3))
+        rows = tf.split(patch, n, axis=0)
+        rows = [tf.concat(tf.unstack(x), axis=1) for x in rows]
+        reconstructed = tf.concat(rows, axis=0)
+        return reconstructed
+
+
+class Patches1(layers.Layer):
+    def __init__(self, patch_size=PATCH_SIZE):
+        super(Patches, self).__init__()
+        self.patch_size = patch_size
+
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
---
+++ missing_modality/TrainMonitor.py
@@ -0,0 +1,53 @@
+# Taking a batch of test inputs to measure model's progress.
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+
+
+class TrainMonitor(tf.keras.callbacks.Callback):
+    def __init__(self, test_ds, epoch_interval=None):
+        self.epoch_interval = epoch_interval
+        self.test_images = next(iter(test_ds))
+
+    def on_epoch_end(self, epoch, logs=None):
+        if self.epoch_interval and epoch % self.epoch_interval == 0:
+            test_augmeneted_images = self.model.test_augmentation_model(self.test_images)
+            test_patches = self.model.patch_layer(test_augmeneted_images)
+            (
+                test_unmasked_embeddings,
+                test_masked_embeddings,
+                test_unmasked_positions,
+                test_mask_indices,
+                test_unmask_indices,
+            ) = self.model.patch_encoder(test_patches)
+            test_encoder_outputs = self.model.encoder(test_unmasked_embeddings)
+            test_encoder_outputs = test_encoder_outputs + test_unmasked_positions
+            test_decoder_inputs = tf.concat(
+                [test_encoder_outputs, test_masked_embeddings], axis=1
+            )
+            test_decoder_outputs = self.model.decoder(test_decoder_inputs)
+
+            # Show a maksed patch image.
+            test_masked_patch, idx = self.model.patch_encoder.show_masked_image(
+                test_patches, test_unmask_indices
+            )
+            print(f"\nIdx chosen: {idx}")
+            original_image = test_augmeneted_images[idx]
+            masked_image = self.model.patch_layer.reconstruct_from_patch(
+                test_masked_patch
+            )
+            reconstructed_image = test_decoder_outputs[idx]
+
+            fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
+            ax[0].imshow(original_image)
+            ax[0].set_title(f"Original: {epoch:03d}")
+
+            ax[1].imshow(masked_image)
+            ax[1].set_title(f"Masked: {epoch:03d}")
+
+            ax[2].imshow(reconstructed_image)
+            ax[2].set_title(f"Resonstructed: {epoch:03d}")
+
+            plt.show()
+            plt.close()
